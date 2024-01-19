3c9a9f2894ddfc60d24e854b8a0a2184ec9847ce
Bharat Ruidas <74824417+brd2002@users.noreply.github.com>
2024-01-19 21:55:34 +0530

    completed task

--- app/src/main/java/com/example/custom_alert_box/MainActivity.kt
+++ app/src/main/java/com/example/custom_alert_box/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.custom_alert_box
 
+import android.annotation.SuppressLint
 import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -9,12 +10,14 @@ import com.example.custom_alert_box.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     lateinit var dialog : Dialog
+    @SuppressLint("ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         dialog = Dialog(this)
         dialog.setContentView(R.layout.activity_custom_aler2)
+        dialog.window?.setBackgroundDrawable(getDrawable(R.drawable.dialog_box_backgroun))
         // create variable for the custom variable
         val GoToNextButton = dialog.findViewById<Button>(R.id.buttonForExit)
         binding.launchAlert.setOnClickListener {
