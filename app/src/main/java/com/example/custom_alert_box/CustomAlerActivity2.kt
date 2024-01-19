191570d246a70ef98d73c80c2fe1bf1bdcea3f1a
Bharat Ruidas <74824417+brd2002@users.noreply.github.com>
2024-01-19 21:48:30 +0530

    completed custom alert button

---
+++ app/src/main/java/com/example/custom_alert_box/CustomAlerActivity2.kt
@@ -0,0 +1,11 @@
+package com.example.custom_alert_box
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CustomAlerActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_custom_aler2)
+    }
+}
--- app/src/main/java/com/example/custom_alert_box/MainActivity.kt
+++ app/src/main/java/com/example/custom_alert_box/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.custom_alert_box
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.custom_alert_box.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    lateinit var dialog : Dialog
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        dialog = Dialog(this)
+        dialog.setContentView(R.layout.activity_custom_aler2)
+        // create variable for the custom variable
+        val GoToNextButton = dialog.findViewById<Button>(R.id.buttonForExit)
+        binding.launchAlert.setOnClickListener {
+            dialog.show()
+        }
+        GoToNextButton.setOnClickListener {
+            dialog.dismiss()
+        }
+
     }
 }
